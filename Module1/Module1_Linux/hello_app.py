724cb191c67e9deda1ea192dc0614f0ef9b09796
Hein Htet Win <heinhtetwin386@gmail.com>
2026-03-28 21:19:04 +0700

    add systemd service

--- Module1/Module1_Linux/hello_app.py
+++ Module1/Module1_Linux/hello_app.py
@@ -5,7 +5,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class HelloWorldHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        message = b"Hello, world!\n"
+        message = b"Hello, world! This is Python App service.\n"
         self.send_response(200)
         self.send_header("Content-Type", "text/plain; charset=utf-8")
         self.send_header("Content-Length", str(len(message)))
